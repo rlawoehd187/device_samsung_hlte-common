c947166700ab52be38fbaa904e8ef17dbc18f04f
Joshua Primero <jprimero155@gmail.com>
2020-02-01 11:47:10 +0000

    hlte-common: Use Light HAL from hardware/samsung
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -83,9 +83,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.MSM8974
+    android.hardware.light@2.0-service.samsung
 
 # Media
 PRODUCT_COPY_FILES += \
