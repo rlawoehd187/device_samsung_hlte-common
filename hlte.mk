9d09196a3650eccd25661efdc104eeafe48d6b98
Joshua Primero <jprimero1555@gmail.com>
2020-04-24 11:18:30 +0000

    hlte-common: Cleanup for AOSP
    * moved some overlay to fw/base
    
    Signed-off-by: Joshua Primero <jprimero1555@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -19,7 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Havoc-OS Official
 export export HAVOC_BUILD_TYPE=Official
