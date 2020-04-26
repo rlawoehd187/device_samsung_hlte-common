85866ff5e35e9ec167d92ffc87f5b11ac4322d42
Joshua Primero <jprimero1555@gmail.com>
2020-04-26 03:54:12 +0000

    hlteh-common: set Shipping API Level
    Signed-off-by: Joshua Primero <jprimero1555@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -125,6 +125,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# Shipping API lvl
+PRODUCT_SHIPPING_API_LEVEL := 18
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-common \
